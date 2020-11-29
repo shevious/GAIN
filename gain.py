9f564c76a36092da7019fcfc233020aaae7d35b1
shevious <shevious@yahoo.com>
2020-11-29 14:57:53 +0900

    gradient tape

--- gain.py
+++ gain.py
@@ -17,6 +17,25 @@ from utils import binary_sampler, uniform_sampler, sample_batch_index
 
 from tensorflow import keras
 
+from tensorflow.keras.layers import Input, Concatenate, Dot, Add, ReLU, Activation
+from tensorflow.keras.layers import Dense
+import tensorflow
+import tensorflow.keras.backend as K
+
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+
+#tf.config.optimizer.set_jit(True)
+
+class GDense(tf.keras.layers.Layer):
+  def __init__(self, w, b):
+    super(GDense, self).__init__()
+    self.w = w
+    self.b = b
+
+  def call(self, x):
+    return tf.matmul(x, self.w) + self.b
+
 class GAIN():
   def __init__(self, dim, alpha):
     self.dim = dim
@@ -24,8 +43,8 @@ class GAIN():
     self.h_dim = int(dim)
 
     # Discriminator variables
-    self.D_W1 = tf.Variable(xavier_init([self.dim*2, self.h_dim])) # Data + Hint as inputs
-    self.D_b1 = tf.Variable(tf.zeros(shape = [self.h_dim]))
+    self.D_W1 = tf.Variable(xavier_init([self.dim*2, self.h_dim]), trainable=True) # Data + Hint as inputs
+    self.D_b1 = tf.Variable(tf.zeros(shape = [self.h_dim]), trainable=True)
     
     self.D_W2 = tf.Variable(xavier_init([self.h_dim, self.h_dim]))
     self.D_b2 = tf.Variable(tf.zeros(shape = [self.h_dim]))
@@ -37,81 +56,268 @@ class GAIN():
 
     #Generator variables
     # Data + Mask as inputs (Random noise is in missing components)
-    self.G_W1 = tf.Variable(xavier_init([self.dim*2, self.h_dim]))  
-    self.G_b1 = tf.Variable(tf.zeros(shape = [self.h_dim]))
+    self.G_W1 = tf.Variable(xavier_init([self.dim*2, self.h_dim]), trainable=True)  
+    self.G_b1 = tf.Variable(tf.zeros(shape = [self.h_dim]), trainable=True)
     
-    self.G_W2 = tf.Variable(xavier_init([self.h_dim, self.h_dim]))
-    self.G_b2 = tf.Variable(tf.zeros(shape = [self.h_dim]))
+    self.G_W2 = tf.Variable(xavier_init([self.h_dim, self.h_dim]), trainable=True)
+    self.G_b2 = tf.Variable(tf.zeros(shape = [self.h_dim]), trainable=True)
     
     self.G_W3 = tf.Variable(xavier_init([self.h_dim, self.dim]))
     self.G_b3 = tf.Variable(tf.zeros(shape = [self.dim]))
     
     self.theta_G = [self.G_W1, self.G_W2, self.G_W3, self.G_b1, self.G_b2, self.G_b3]
 
-  ## GAIN functions
-  # Generator
-  def generator(self, x, m):
-    # Concatenate Mask and Data
-    inputs = tf.concat(values = [x, m], axis = 1) 
-    G_h1 = tf.nn.relu(tf.matmul(inputs, self.G_W1) + self.G_b1)
-    G_h2 = tf.nn.relu(tf.matmul(G_h1, self.G_W2) + self.G_b2)   
-    # MinMax normalized output
-    G_prob = tf.nn.sigmoid(tf.matmul(G_h2, self.G_W3) + self.G_b3) 
-    return G_prob
-      
-  # Discriminator
-  def discriminator(self, x, h):
-    # Concatenate Data and Hint
-    inputs = tf.concat(values = [x, h], axis = 1) 
-    D_h1 = tf.nn.relu(tf.matmul(inputs, self.D_W1) + self.D_b1)  
-    D_h2 = tf.nn.relu(tf.matmul(D_h1, self.D_W2) + self.D_b2)
-    D_logit = tf.matmul(D_h2, self.D_W3) + self.D_b3
-    D_prob = tf.nn.sigmoid(D_logit)
-    return D_prob
+    self.build_generator()
+    #self.generator.summary()
+    self.build_discriminator()
+    #self.discriminator.summary()
+    #self.build_adversarial()
+    #self.model.summary()
 
-  @tf.function
-  def D_fun(self, M, X, H):
-    self.M = M
+  ## GAIN models
+  def build_generator(self):
+
+    xavier_initializer = tf.keras.initializers.GlorotNormal()
+
+    x = Input(shape=(self.dim,), name='generator_input_x')
+    m = Input(shape=(self.dim,), name='generator_input_m')
+
+    a = Concatenate()([x, m])
+
+    a = Dense(self.h_dim, activation='relu', kernel_initializer=xavier_initializer)(a)
+    a = Dense(self.h_dim, activation='relu', kernel_initializer=xavier_initializer)(a)
+    G_prob = Dense(self.dim, activation='sigmoid', kernel_initializer=xavier_initializer)(a)
+    self.generator = keras.models.Model([x, m], G_prob, name='generator')
+
+  def build_discriminator(self):
+
+    xavier_initializer = tf.keras.initializers.GlorotNormal()
+
+    x = Input(shape=(self.dim,), name='discriminator_input_x')
+    h = Input(shape=(self.dim,), name='discriminator_input_h')
+
+    a = Concatenate()([x, h])
+
+    a = Dense(self.h_dim, activation='relu', kernel_initializer=xavier_initializer)(a)
+    a = Dense(self.h_dim, activation='relu', kernel_initializer=xavier_initializer)(a)
+    D_prob = Dense(self.dim, activation='sigmoid', kernel_initializer=xavier_initializer)(a)
+    self.discriminator = keras.models.Model([x, h], D_prob, name='discriminator')
+
+  def set_trainable(self, m, val):
+    m.trainable = val
+    for l in m.layers:
+      l.trainable = val
+
+  def G_loss_bincross(M, D_prob):
+    G_loss_temp = -tf.reduce_mean((1-M) * tf.keras.backend.log(D_prob + 1e-8))
+    #MSE_loss = tf.reduce_mean((M * X - M * G_sample)**2) / tf.reduce_mean(M)
+    #G_loss = G_loss_temp + self.alpha * MSE_loss 
+    #return G_loss
+    return G_loss_temp
+
+  def MSE_loss(M, X, G_sample):
+    MSE_loss = tf.reduce_mean((M * X - M * G_sample)**2) / tf.reduce_mean(M)
+    return MSE_loss
+
+  def G_loss(self, M, D_prob, X, G_sample):
+    G_loss_temp = -tf.reduce_mean((1-M) * tf.keras.backend.log(D_prob + 1e-8))
+    MSE_loss = tf.reduce_mean((M * X - M * G_sample)**2) / tf.reduce_mean(M)
+    #G_loss_temp = GAIN.G_loss_bincross(M, D_prob)
+    #MSE_loss = GAIN.MSE_loss(M, X, G_sample)
+    G_loss = G_loss_temp + self.alpha * MSE_loss 
+    return G_loss
+
+  def build_adversarial(self):
+
+    ### COMPILE DISCRIMINATOR
+
+    self.discriminator.compile(
+      optimizer=Adam(),
+      #loss = 'binary_crossentropy',
+      loss = self.D_loss(),
+      #metrics = ['accuracy']
+      metrics = ['binary_accuracy']
+    )
+
+    ### COMPILE THE FULL GAN
+
+    self.set_trainable(self.discriminator, False)
+
+    X = Input(shape=(self.dim,), name='model_input_x')
+    M = Input(shape=(self.dim,), name='model_input_m')
+    H = Input(shape=(self.dim,), name='model_input_h')
+
+    model_input = [X, M, H]
+    G_sample = self.generator([X, M])
     self.X = X
-    self.H = H
-
-    ## GAIN structure
-    # Generator
-    self.G_sample = self.generator(X, M)
 
     # Combine with observed data
-    self.Hat_X = X * M + self.G_sample * (1-M)
-    
-    # Discriminator
-    self.D_prob = self.discriminator(self.Hat_X, H)
+    Hat_X = X * M + G_sample * (1-M)
+    D_prob = self.discriminator([Hat_X, H])
+    model_output = D_prob
+    self.model = Model(model_input, model_output)
 
-    ## GAIN loss
-    D_loss_temp = -tf.reduce_mean(M * tf.keras.backend.log(self.D_prob + 1e-8) \
-                                  + (1-M) * tf.keras.backend.log(1. - self.D_prob + 1e-8)) 
+    self.model.add_loss(self.G_loss(M, D_prob, X, G_sample))
+    #self.model.add_metric(GAIN.G_loss_bincross(M, D_prob), name='binary_crossentropy')
+    #self.model.add_metric(GAIN.MSE_loss(M, X, G_sample), name='MSE_loss')
 
-    self.D_loss = D_loss_temp
-    return self.D_loss
-    
+    self.model.compile( 
+      optimizer=Adam(),
+      #loss='binary_crossentropy',
+      #loss=self.G_loss(),
+      loss=None,
+      metrics=['binary_accuracy']
+    )
+    self.model.summary()
+    #print('### build')
+    #print(len(self.model.inputs))
+    #print(self.model.inputs[0].shape)
+    #print(len(self.model.outputs))
+    #print(self.model.outputs[0].shape)
+    self.set_trainable(self.discriminator, True)
+
+    '''
+    self.tensorboard = tf.keras.callbacks.TensorBoard(
+        log_dir='logs', histogram_freq=0, write_graph=True, write_images=False,
+        update_freq='epoch', profile_batch=2, embeddings_freq=0,
+        embeddings_metadata=None,
+    )
+    self.tensorboard.set_model(self.discriminator)
+    '''
+
+  # Transform train_on_batch return value
+  # to dict expected by on_batch_end callback
+  def named_logs(model, logs):
+    result = {}
+    for l in zip(model.metrics_names, logs):
+      result[l[0]] = l[1]
+    return result
+
+  def D_loss(y_true, y_pred):
+      M = y_true
+      D_prob = y_pred
+      ## GAIN loss
+      d_loss = -tf.reduce_mean(M * tf.keras.backend.log(D_prob + 1e-8) \
+                    + (1-M) * tf.keras.backend.log(1. - D_prob + 1e-8)) 
+      return d_loss
+      #return { 'loss': self.D_loss, 'myloss': 0 }
+
+  # `tf.function`이 어떻게 사용되는지 주목해 주세요.
+  # 이 데코레이터는 함수를 "컴파일"합니다.
   @tf.function
-  def G_fun(self, X, M, H):
+  def train_step(self, inputs):
+      X, M, H = inputs
+      with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        G_sample = self.generator([X, M], training=True)
+
+        # Combine with observed data
+        Hat_X = X * M + G_sample * (1-M)
+        D_prob = self.discriminator([Hat_X, H], training=True)
+        gen_loss = self.G_loss(M, D_prob, X, G_sample)
+        disc_loss = GAIN.D_loss(M, D_prob)
+
+      gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+      gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+
+      self.generator_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
+      self.discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
+
+  def train_discriminator(self, M, X, H):
+
     ## GAIN structure
+
     # Generator
-    G_sample = self.generator(X, M)
+    #import time
+    #tick = time.time()
+    G_sample = self.generator.predict([X, M])
+    #tock = time.time()
+    #print('disc gen time = ', (tock-tick)*1000)
 
     # Combine with observed data
     Hat_X = X * M + G_sample * (1-M)
 
     # Discriminator
-    D_prob = self.discriminator(Hat_X, H)
+    #tick = time.time()
+    return self.discriminator.train_on_batch([Hat_X, H], M)
+    #tock = time.time()
+    #print('disc train time = ', (tock-tick)*1000)
 
-    G_loss_temp = -tf.reduce_mean((1-M) * tf.keras.backend.log(D_prob + 1e-8))
-    
-    self.MSE_loss = \
-    tf.reduce_mean((M * X - M * G_sample)**2) / tf.reduce_mean(M)
+  def train_generator(self, X, M, H):
 
-    self.G_loss = G_loss_temp + self.alpha * self.MSE_loss 
-    return self.G_loss
-    
+    ## GAIN structure
+
+    # Generator
+    #import time
+    #tick = time.time()
+    #self.model.train_on_batch([X, M, H], [M, X])
+    #tock = time.time()
+    #print('gen train time = ', (tock-tick)*1000)
+    return self.model.train_on_batch([X, M, H], M)
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, data_x, batch_size, hint_rate):
+        'Initialization'
+        self.no, self.dim = data_x.shape
+        self.batch_size = batch_size
+        self.hint_rate = hint_rate
+
+        # Define mask matrix
+        self.data_m = 1-np.isnan(data_x)
+
+        # Normalization
+        self.norm_data, self.norm_parameters = normalization(data_x)
+        self.norm_data_x = np.nan_to_num(self.norm_data, 0)
+        #norm_data_x = norm_data_x.astype(np.float32)
+        #data_x = data_x.astype(np.float32)
+
+        self.idx = 0
+        self.batch_cnt = self.no // self.batch_size
+        self.shuffle_idx = np.random.permutation(self.no)
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        #return int(128/self.batch_size)
+        #return 2
+        return 1
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        #print('index =', index)
+        # Sample batch
+        #batch_idx = sample_batch_index(self.no, self.batch_size)
+        batch_idx = self.shuffle_idx[self.idx*self.batch_size:(self.idx+1)*self.batch_size]
+        self.idx = (self.idx+1)%self.batch_cnt
+        if (self.idx == 0):
+            self.shuffle_idx = np.random.permutation(self.no)
+        X_mb = self.norm_data_x[batch_idx, :]  
+        M_mb = self.data_m[batch_idx, :]  
+        #X_mb = tf.convert_to_tensor(X_mb, dtype=tf.float32)
+        #M_mb = tf.convert_to_tensor(M_mb, dtype=tf.float32)
+        
+        # Sample random vectors  
+        Z_mb = uniform_sampler(0, 0.01, self.batch_size, self.dim)
+        #Z_mb = tf.keras.backend.random_uniform(shape=(self.batch_size,self.dim), minval=0.0, maxval=0.01)
+        # Sample hint vectors
+        H_mb_temp = binary_sampler(self.hint_rate, self.batch_size, self.dim)
+        #print('H_mb_temp.shape=',H_mb_temp.shape)
+        #print('M_mb.shape=', M_mb.shape)
+
+        H_mb = M_mb * H_mb_temp
+          
+        # Combine random vectors with observed vectors
+        X_mb = M_mb * X_mb + (1-M_mb) * Z_mb 
+
+        return X_mb, M_mb, H_mb
+        #return {
+          #'model_input_x': X_mb,
+          #'model_input_m': M_mb,
+          #'model_input_h': H_mb,
+        #}
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        return
 
 def gain (data_x, gain_parameters):
   '''Impute missing values in data_x
@@ -143,21 +349,92 @@ def gain (data_x, gain_parameters):
   h_dim = int(dim)
   
   # Normalization
-  print('###')
-  print('data_x = ', data_x)
   norm_data, norm_parameters = normalization(data_x)
   norm_data_x = np.nan_to_num(norm_data, 0)
   norm_data_x = norm_data_x.astype(np.float32)
   data_x = data_x.astype(np.float32)
-  
-  opt_D = tf.keras.optimizers.Adam()
-  opt_G = tf.keras.optimizers.Adam()
 
+  train_generator = DataGenerator(data_x, batch_size, hint_rate)
+  it = iter(train_generator)
+
+  ''' check
+  #(x_mb, m_mb, h_mb) = train_generator.__getitem__(0)
+  (x_mb, m_mb, h_mb) = next(it)
+  print(x_mb.shape, x_mb.dtype)
+  print(m_mb.shape, m_mb.dtype)
+  print(h_mb.shape, m_mb.dtype)
+
+  (x_mb, m_mb, h_mb) = next(it)
+  (x_mb, m_mb, h_mb) = next(it)
+  import sys
+  sys.exit(0)
+  '''
+  ds = tf.data.Dataset.from_generator(
+      lambda: train_generator.__iter__(),
+      output_types=(tf.float32, tf.float32, tf.float32),
+      output_shapes=(
+        [batch_size, train_generator.dim],
+        [batch_size, train_generator.dim],
+        [batch_size, train_generator.dim],
+      )
+  ).repeat(-1).prefetch(10)
+  ''' check ds
+  print(ds.element_spec)
+  it_ds = iter(ds)
+  print(ds.element_spec)
+  for x_mb, m_mb, h_mb in ds.take(1):
+    print(x_mb.shape, x_mb.dtype)
+    print(m_mb.shape, m_mb.dtype)
+    print(h_mb.shape, m_mb.dtype)
+    print(x_mb.numpy())
+    print(m_mb.numpy())
+    print(h_mb.numpy())
+  it = iter(ds)
+  x_mb, m_mb, h_mb = next(it)
+
+  x_mb, m_mb, h_mb = next(it)
+
+  x_mb, m_mb, h_mb = next(it)
+  '''
+  it = iter(ds)
+  X_mb, M_mb, H_mb = next(it)
+  
+  ## Iterations
   gain = GAIN(dim, alpha)
+  gain.generator_optimizer = Adam()
+  gain.discriminator_optimizer = Adam()
+
+  it_ds = iter(ds)
+
+  #import sys
+  #sys.exit(1)
+
+  #warm up
+  #gain.model.fit(ds, epochs=3, batch_size=128)
+
+  #import sys
+  #sys.exit(0)
+
+  import time
+  #tick = time.time()
+  #gain.model.fit(ds, epochs=100, batch_size=128)
+  #tock = time.time()
+  #print('model fit=', (tock-tick)*1000)
+
+  #warm up
+  for i in range(10):
+    X_mb, M_mb, H_mb = next(it_ds)
+    gain.train_step([X_mb, M_mb, H_mb])
    
+  #tf.profiler.experimental.start('logdir')
   # Start Iterations
-  for it in tqdm(range(iterations)):    
+  progress = tqdm(range(iterations))
+  for it in progress:
+  #for it in range(iterations):    
+    X_mb, M_mb, H_mb = next(it_ds)
+    gain.train_step([X_mb, M_mb, H_mb])
       
+    '''
     # Sample batch
     batch_idx = sample_batch_index(no, batch_size)
     X_mb = norm_data_x[batch_idx, :]  
@@ -181,27 +458,41 @@ def gain (data_x, gain_parameters):
     #print('Z_mb.ndtype = ', Z_mb.dtype)
     #print('norm_data_x.ndtype = ', norm_data_x.dtype)
 
-    import time
-    tick = time.time()
-    loss = opt_D.minimize(lambda: gain.D_fun(M_mb, X_mb, H_mb), var_list = gain.theta_D)
-    D_loss_curr = gain.D_loss
-    tock = time.time()
-    print('D = ', (tock-tick)*1000)
+    #loss = opt_D.minimize(lambda: gain.D_fun(M_mb, X_mb, H_mb), var_list = gain.theta_D)
+    '''
+    #X_mb, M_mb, H_mb = next(it_ds)
 
-    tick = time.time()
-    loss = opt_G.minimize(lambda: gain.G_fun(X_mb, M_mb, H_mb), var_list = gain.theta_G)
-    tock = time.time()
-    print('G = ', (tock-tick)*1000)
+    #import time
+    #d_logs = gain.train_discriminator(M_mb, X_mb, H_mb)
+    #print(gain.discriminator.metrics_names)
+    #print(logs)
+    #gain.tensorboard.on_epoch_end(it, GAIN.named_logs(gain.discriminator, logs))
+    #print('disc time = ', (tock-tick)*1000)
+    #D_loss_curr = gain.D_loss
+
+    #loss = opt_G.minimize(lambda: gain.G_fun(X_mb, M_mb, H_mb), var_list = gain.theta_G)
+    #tick = time.time()
+    #g_logs = gain.train_generator(X_mb, M_mb, H_mb)
+
+    #g_logs = gain.model.fit([X_mb, M_mb, H_mb])
+
+    #progress.set_description('d_loss: %f, g_loss: %f' % (d_logs[0], g_logs[0]))
+    #print(gain.model.metrics_names)
+    #print(logs)
+    #tock = time.time()
+    #print('gen time = ', (tock-tick)*1000)
     
-    G_loss_curr = gain.G_loss
-    MSE_loss_curr = gain.MSE_loss
+    #G_loss_curr = gain.G_loss
+    #MSE_loss_curr = gain.MSE_loss
 
     #_, D_loss_curr = sess.run([D_solver, D_loss_temp], 
                               #feed_dict = {M: M_mb, X: X_mb, H: H_mb})
     #_, G_loss_curr, MSE_loss_curr = \
     #sess.run([G_solver, G_loss_temp, MSE_loss],
              #feed_dict = {X: X_mb, M: M_mb, H: H_mb})
+  #tf.profiler.experimental.stop()
             
+  #gain.tensorboard.on_train_end(None)
   ## Return imputed data      
   Z_mb = uniform_sampler(0, 0.01, no, dim) 
   M_mb = data_m
@@ -211,8 +502,8 @@ def gain (data_x, gain_parameters):
   #imputed_data = sess.run([G_sample], feed_dict = {X: X_mb, M: M_mb})[0]
   X_mb = X_mb.astype(np.float32)
   M_mb = M_mb.astype(np.float32)
-  imputed_data = gain.generator(X_mb, M_mb)
-  imputed_data = imputed_data.numpy()
+  imputed_data = gain.generator.predict([X_mb, M_mb])
+  #imputed_data = imputed_data.numpy()
   
   imputed_data = data_m * norm_data_x + (1-data_m) * imputed_data
   
